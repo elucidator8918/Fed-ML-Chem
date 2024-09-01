2d1b7254963806710cbc14e882c9423f7bc9d16f
Siddhant <forsomethingnewsid@gmail.com>
2024-09-01 20:01:11 +0530

    Update run-cpu.sh

--- run-cpu.sh
+++ run-cpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip install "flwr==1.5.0" tqdm numpy pennylane "ray>=2.3.0" matplotlib pillow scikit-learn seaborn pandas opacus pyyaml tenseal kaggle sentence_transformers
+pip install "flwr==1.5.0" tqdm numpy pennylane "ray>=2.3.0" matplotlib pillow scikit-learn seaborn pandas pyyaml kaggle sentence_transformers
 pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cpu
