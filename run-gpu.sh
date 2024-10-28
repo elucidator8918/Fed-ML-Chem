337463680b7ac75c751f1e71f9b6be3928b232dd
Siddhant <forsomethingnewsid@gmail.com>
2024-10-28 19:04:29 +0530

    Update run-gpu.sh

--- run-gpu.sh
+++ run-gpu.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 pip install "flwr==1.5.0" tqdm numpy pennylane "ray>=2.3.0" matplotlib pillow scikit-learn seaborn pandas pyyaml kaggle sentence_transformers torch torchvision torchaudio torch-geometric rdkit
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.4.0+cu121.html
